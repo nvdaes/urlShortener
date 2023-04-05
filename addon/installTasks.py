e93a2226b906b663394bfafdcb0ecc6295759a0a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-04-05 18:58:56 +0200

    Use installTasks to add and remove addon config path

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -4,27 +4,22 @@
 # Copyright (C) 2023 Noelia Ruiz Martínez, other contributors
 # Released under GPL2
 
-import json
 import os
 
 import addonHandler
 import globalVars
 
-ADDON_DIR = os.path.abspath(os.path.dirname(__file__))
-URLS_PATH = os.path.join(ADDON_DIR, "globalPlugins", "urlShortener")
 CONFIG_PATH = globalVars.appArgs.configPath
-
-addonHandler.initTranslation()
+ADDON_CONFIG_PATH = os.path.join(CONFIG_PATH, "urlShortener")
 
 
 def onInstall():
-	previousUrlsPath = os.path.join(
-		CONFIG_PATH, "addons", "urlShortener",
-		"globalPlugins", "urlShortener", "urls.json"
-	)
-	if not os.path.isfile(previousUrlsPath):
-		return
-	with open(previousUrlsPath, "rt") as f:
-		data = json.load(f)
-	with open(URLS_PATH, "wt") as f:
-		json.dump(data, f, indent="\t")
+	if not os.path.isdir(ADDON_CONFIG_PATH):
+		os.makedirs(ADDON_CONFIG_PATH)
+
+
+def onUninstall():
+	try:
+		os.removedirs(ADDON_CONFIG_PATH)
+	except Exception as e:
+		raise e
