bae041332248925817dbfa4584cf00af6870ed64
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-06-27 10:01:35 +0200

    Fix translator comment

--- addon/globalPlugins/urlShortener/urlsGui.py
+++ addon/globalPlugins/urlShortener/urlsGui.py
@@ -245,6 +245,7 @@ class UrlsDialog(wx.Dialog):
 		self.sel = self.urlsList.Selection
 		self.stringSel = self.urlsList.GetString(self.sel)
 		url = self._urls[self.filteredItems[self.sel]]
+		# Translators: Info about the selected URL.
 		urlInfo = _(
 			"Original URL: {}\n"
 			# Translators: Info about the selected URL.
